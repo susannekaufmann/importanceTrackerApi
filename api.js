5600347c58d5abb5887a3eb9144fe1428600277e
Susanne Kaufmann <susanne@Susannes-new-Mac.local>
2017-09-02 22:11:33 -0700

    "first rest api commit"

---
+++ api.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.port || 3000;
+var router = express.Router();
+
+
+app.use('/api/task', router);
+app.listen(port);
+
+/**
+ * serves as our database for now.
+ */
+var tasks = [{
+    'id': 1,
+    'task': 'Create ImportanceTracker API',
+    'dateAdded': 'September 2, 2017 03:24:00',
+    'dateUpdated': 'September 2, 2017 03:24:00',
+    'dateDue': 'December 2, 2017 03:24:00',
+    'communicationType': 'igotthis',
+    'taskType': 'strategic'
+}];
+
+/* get all tasks */
+router.get('/', function(req, res) {
+    res.json(tasks);
+});
+
+/* get specific task */
+router.get('/:id', function(req, res) {
+    var taskId = parseInt(req.params.id);
+    /* filter by taskId; and show the first item in response array [0] */
+    var currentTask = tasks.filter(e => e.id === taskId)[0];
+
+    if (currentTask) {
+        res.json(currentTask);
+    } else {
+        res.sendStatus(404);
+    }
+});
